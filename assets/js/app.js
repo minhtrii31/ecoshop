3479ac71a5b54b29dce2dd7102177e316b09228b
minhtrii31 <86295254+minhtrii31@users.noreply.github.com>
2022-01-14 12:01:38 +0000

    update (change name of img flle but no sync)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,1 @@
-$(document).ready(function () {
-  $(".body-list-item-thumn-img").hover(
-    function () {
-      var scrDefault = $(this).src;
-      $(this).attr("src", scrDefault + "_b");
-    },
-    function () {
-      $(this).attr("src", scrDefault);
-    }
-  );
-});
+// Empty
---
+++ assets/js/product.js
@@ -0,0 +1,21 @@
+$(".product-slider-img").click(function () {
+  var dSrc = $(this).attr("src");
+  $(".product-slider-img").css("border", "none");
+  $(this).css("border", "1px solid black");
+  console.log(dSrc);
+  $("#img").attr("src", dSrc);
+});
+$("#seeMore").click(function () {
+  $(this).css("display", "none");
+  $(".hidden").slideToggle("slow", function () {
+    $(".hidden").css("display", "block");
+  });
+  $("#seeLess").css("display", "block");
+});
+$("#seeLess").click(function () {
+  $(this).css("display", "none");
+  $(".hidden").slideToggle("slow", function () {
+    $(".hidden").css("display", "none");
+  });
+  $("#seeMore").css("display", "block");
+});
