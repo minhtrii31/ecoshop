a523f8b356b64999e7b2766d6e06b704f4702247
minhtrii31 <minhtri3101200@gmail.com>
2022-01-23 23:04:45 +0700

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-// Empty
+// Show category
+function showCategory() {
+  var check = document.getElementById("nav").style.display;
+  if (check == "block") {
+    document.getElementById("nav").style.display = "none";
+    document
+      .getElementById("more")
+      .setAttribute("name", "ellipsis-vertical-outline");
+  } else {
+    document.getElementById("nav").style.display = "block";
+    document.getElementById("more").setAttribute("name", "close-outline");
+  }
+}
