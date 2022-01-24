fd4b589b992ee7107cc21720acdbd7ad13e7a190
minhtrii31 <minhtri3101200@gmail.com>
2022-01-24 15:17:22 +0700

    fix width, height

---
+++ assets/js/store.js
@@ -0,0 +1,21 @@
+// Change location
+$(document).ready(function () {
+  $("#l1").click(function () {
+    $("#map").attr(
+      "src",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4355914971993!2d106.70093807637069!3d10.777911907818371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f4807554a53%3A0xd621c23149b3aa5d!2zQmFvVmlldCBCdWlsZGluZywgMjMzIMSQ4buTbmcgS2jhu59pLCBC4bq_biBOZ2jDqSwgUXXhuq1uIDEsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1642939986745!5m2!1sen!2s"
+    );
+  });
+  $("#l2").click(function () {
+    $("#map").attr(
+      "src",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4090273038123!2d106.69575951524104!3d10.779951662075318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f37bc37b8f1%3A0x32cf32084c0e2d66!2zMTgyIEzDqiBEdeG6qW4sIELhur9uIE5naMOpLCBRdeG6rW4gMSwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1643007927257!5m2!1sen!2s"
+    );
+  });
+  $("#l3").click(function () {
+    $("#map").attr(
+      "src",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.1863616414576!2d106.69147681524096!3d10.7970342617643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528ced20e5a39%3A0x65e4878f3d75dc14!2zMSBQaGFuIFjDrWNoIExvbmcsIFBoxrDhu51uZyAzLCBCw6xuaCBUaOG6oW5oLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1643007988209!5m2!1sen!2s"
+    );
+  });
+});
